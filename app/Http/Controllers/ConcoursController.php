3c692799e370e64aee5427886c30a3ff7ab73bc3
mzunaid9 <zunaid.mougaidinessaib9@gmail.com>
2025-03-19 14:04:56 +0000

    modification controller pour afficher les epreuves

--- app/Http/Controllers/ConcoursController.php
+++ app/Http/Controllers/ConcoursController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\concours;
+use App\Models\epreuve;
 use Illuminate\Http\Request;
 
 class ConcoursController extends Controller
@@ -10,5 +11,10 @@ class ConcoursController extends Controller
     public function index(){
         $listeConcours = concours::all();
         return view('index',compact("listeConcours"));
+    }
+
+    public function allEpreuves(){
+        $listeEpreuve = epreuve::all();
+        return view('index',compact("listeEpreuve"));
     }
 }
