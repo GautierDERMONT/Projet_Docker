81ab80b43c5cbab95ad97adff99de374941af17a
mzunaid9 <zunaid.mougaidinessaib9@gmail.com>
2025-03-19 14:44:46 +0100

    relation OneToMany entre model Concours et Epreuve

--- app/Models/epreuve.php
+++ app/Models/epreuve.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class epreuve extends Model
 {
     use HasFactory;
+
+    public function concours(): BelongsTo
+    {
+        return $this->belongsTo(concours::class);
+    }
 }
